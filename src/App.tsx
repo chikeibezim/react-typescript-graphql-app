09381aeacf5df44e0a6e81819be5e0f57b97a722
Chike Ibezim <ibezimchike@gmail.com>
2023-01-10 12:48:19 +0100

    connect graphql client and query database

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,22 @@
-import { User } from './types';
+import { useQuery } from 'urql';
+import { GetUsersDocument } from './graphql/generated';
 import UserDisplay from './components/UserDisplay';
 
 function App(){
 
-  const users: User[] = [{
-    name: `Prisma Fan`,
-    messages: [
-      {
-        body: "Prisma Rocks!"
-      },
-      {
-        body: "Did i mention i love Prisma"
-      },
-      {
-        body: "Did i mention i love Prisma"
-      }
-    ]
-  }];
+  //this uses the GeUserDocument query object to request data from your api and return
+  //it in a properly typed variable
+  //we no longer need the user type mport because the typngs is already bering specified
+  //in the GetUsersDocument object.
+
+  const [results] = useQuery({
+    query: GetUsersDocument
+  })
 
   return(
     <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">
       {
-        users.map((user, i) => {
+        results.data?.users.map((user, i) => {
           return (
             <UserDisplay user={user} key={i} />
           )
--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,15 @@
-export type Message = {
-    body: String
-}
+import type { GetUsersQuery } from "./graphql/generated";
 
-export type User = {
-    name: String,
-    messages: Message[]
-}
+/*
+The reason you import this type instead of the full User and Message types is that
+ the GetUsersQuery type has access to a more specific set of types that contain 
+ only the fields your query retrieves.
+*/
+
+/*
+Replace the existing types with the following to expose the types
+representing your query results:
+*/
+
+export type Message = GetUsersQuery["users"][0]["messages"][0];
+export type User = GetUsersQuery["users"][0];
